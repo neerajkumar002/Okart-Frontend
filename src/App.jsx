4d24394d490f0189a76e887e595c9160b0205d99
Neeraj kumar <neerajkumar08697@gmail.com>
2024-08-10 12:55:11 +0530

    add react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
 function App() {
-  return <>Hello</>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
