54eac88dcf45775430064c782bfeea10885c53e9
Neeraj Kumar <neeraj.dev002@gmail.com>
2024-08-13 12:28:06 +0530

    add list

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { Link, useLocation } from "react-router-dom";
 import { RiDashboardFill, RiShoppingBag3Fill } from "react-icons/ri";
 import { AiFillFileText } from "react-icons/ai";
@@ -14,58 +15,47 @@ const AdminSidebar = () => {
       <div>
         <h5>Dashboard</h5>
         <ul>
-          <li
-            style={{
-              backgroundColor: location.pathname.includes("/admin/dashboard")
-                ? "rgba(0,115,255,0.1)"
-                : "white",
-            }}
-          >
-            <Link to="/admin/dashboard">
-              <RiDashboardFill />
-              Dashboard
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor: location.pathname.includes("/admin/product")
-                ? "rgba(0,115,255,0.1)"
-                : "white",
-            }}
-          >
-            <Link to="/admin/product">
-              <RiShoppingBag3Fill />
-              Product
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor: location.pathname.includes("/admin/customer")
-                ? "rgba(0,115,255,0.1)"
-                : "white",
-            }}
-          >
-            <Link to="/admin/customer">
-              <IoIosPeople />
-              Customer
-            </Link>
-          </li>
-          <li
-            style={{
-              backgroundColor: location.pathname.includes("/admin/transaction")
-                ? "rgba(0,115,255,0.1)"
-                : "white",
-            }}
-          >
-            <Link to="/admin/transaction">
-              <AiFillFileText />
-              Transaction
-            </Link>
-          </li>
+          <Li
+            text={"Dashboard"}
+            Icon={RiDashboardFill}
+            url="/admin/dashboard"
+          />
+          <Li
+            text={"Product"}
+            Icon={RiShoppingBag3Fill}
+            url="/admin/product"
+          />
+          <Li
+            text={"Customer"}
+            Icon={IoIosPeople}
+            url="/admin/customer"
+          />
+          <Li
+            text={"Transaction"}
+            Icon={AiFillFileText}
+            url="/admin/transaction"
+          />
+
+           
         </ul>
       </div>
     </aside>
   );
 };
+
+const Li = ({ text, Icon, url }) => (
+  <li
+    style={{
+      backgroundColor: location.pathname.includes(url)
+        ? "rgba(0,115,255,0.1)"
+        : "white",
+    }}
+  >
+    <Link to={url}>
+      <Icon />
+      {text}
+    </Link>
+  </li>
+);
 
 export default AdminSidebar;
